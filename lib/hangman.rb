150d27f8edc62e7ebe10cc38d408a1f34eb7a4c1
marlisebriedenhann <marlise.briedenhann@envato.com>
2020-01-22 16:39:56 +1100

    Add script to call hangman

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,6 +4,6 @@ class Hangman
   attr_reader :masked_word
 
   def initialize(new_word:)
-    @masked_word = new_word.chars.map { '_' }.join.to_s
+    @masked_word = new_word.chars.map { '_ ' }.join.to_s
   end
 end
